3c7975965a8a855e148cdd3195512f29ae961d26
Vinay Joshi <vinay01joshi@gmail.com>
2021-09-20 12:38:31 +0530

    Adding Bootstrap to design component.

--- src/components/collection-item.tsx
+++ src/components/collection-item.tsx
@@ -1,8 +1,21 @@
 import React from "react";
+import { Button, Card } from "react-bootstrap";
 import Car from "../models/car";
 
 const CollectionItem: React.FunctionComponent<Car> = (car) => {
-  return <div> {car.name} </div>;
+  return (
+    <>
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src={car.image} />
+        <Card.Body>
+          <Card.Title>{car.name}</Card.Title>
+          <Card.Text>{car.price}</Card.Text>
+          <Button variant="primary">Add To cart</Button>
+        </Card.Body>
+      </Card>
+    </>
+  );
+  // return <div> {car.name} </div>;
 };
 
 export default CollectionItem;
--- src/pages/shop-page.tsx
+++ src/pages/shop-page.tsx
@@ -1,25 +1,52 @@
 import CollectionItem from "../components/collection-item";
 import Car from "../models/car";
+import "./shop-page.css";
 
 const ShopPage = () => {
   const cars: any = [
-    { id: 1, name: "Audi", price: 50000, image: "" },
-    { id: 2, name: "Mercedes", price: 55000, image: "" },
-    { id: 3, name: "Skoda", price: 9000, image: "" },
-    { id: 4, name: "Volvo", price: 50000, image: "" },
+    {
+      id: 1,
+      name: "Audi",
+      price: 50000,
+      image:
+        "https://www.setaswall.com/wp-content/uploads/2017/11/Audi-A8-Wallpaper-20-1920x1080.jpg",
+    },
+    {
+      id: 2,
+      name: "Mercedes",
+      price: 55000,
+      image:
+        "http://www.thewowstyle.com/wp-content/uploads/2015/02/Audi-cars-wallpaper.jpg",
+    },
+    {
+      id: 3,
+      name: "Skoda",
+      price: 9000,
+      image:
+        "https://www.automobilians.com/wp-content/uploads/2020/05/2020-Skoda-Karoq-India-Launch-2-scaled.jpg",
+    },
+    {
+      id: 4,
+      name: "Volvo",
+      price: 50000,
+      image:
+        "https://amcdn.blob.core.windows.net/media/1/volvo-manufacturer-profile--march-2018-/211732-xc60-t8-inscription.jpg",
+    },
   ];
 
   return (
     <div className="shop">
       <h1>Shop Page</h1>
-      {cars.map((car: Car) => (
-        <CollectionItem
-          id={car.id}
-          name={car.name}
-          price={car.price}
-          image={car.image}
-        />
-      ))}
+      <div className="collection-preview">
+        {cars.map((car: Car) => (
+          <CollectionItem
+            id={car.id}
+            name={car.name}
+            price={car.price}
+            image={car.image}
+          />
+        ))}
+      </div>
     </div>
   );
 };
